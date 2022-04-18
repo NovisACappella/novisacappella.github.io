c021ba294fe15f6e11654d6429e5a3868889adf5
LudoLogical <dcd180001@utdallas.edu>
2022-04-18 16:06:27 -0500

    Added movie titles

--- dynamicconcert.js
+++ dynamicconcert.js
@@ -1,5 +1,6 @@
 var expand = function () {
-    $("#filler").height(Math.max(50, $(window).height() - $("#letterboard").height() - $("#popcorn").height() - parseInt($("#splash").css("padding-top"))));
+    $("#filler").height(Math.max(50, $(window).height() - 254 - 515 - 50));
+    //$("#filler").height(Math.max(50, $(window).height() - $("#letterboard").height() - $("#popcorn").height() - parseInt($("#splash").css("padding-top"))));
 }
 
 $(document).ready(function(){
